39909d3b26e9256772dfe7ad985d5bf61807e794
azaman5025 <a.zaman5025@gmail.com>
2023-05-07 00:09:07 +0200

    commit all the dependency

---
+++ src/main/java/com/awsdeploymenttest/awsdeptest/AwsDepTestApplication.java
@@ -0,0 +1,15 @@
+package com.awsdeploymenttest.awsdeptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDepTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsDepTestApplication.class, args);
+    }
+
+}
+
+
---
+++ src/test/java/com/awsdeploymenttest/awsdeptest/AwsDepTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsdeploymenttest.awsdeptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDepTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
