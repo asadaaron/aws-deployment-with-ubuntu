9596a2fb7005726fe1ab25bde775232f4b9e671a
azaman5025 <a.zaman5025@gmail.com>
2023-05-07 02:29:19 +0200

    added docker information

---
+++ src/main/java/com/awsdeploymenttest/awsdeptest/controller.java
@@ -0,0 +1,17 @@
+package com.awsdeploymenttest.awsdeptest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "This is a trial.";
+    }
+
+}
